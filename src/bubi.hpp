2e95a3a3172115bea4ea92123a780ff90ae0889f
ego <ego@localhost>
2026-03-31 19:47:31 +0200

    LCD Ausgabe integriert. Quelltexte aufgeräumt. FINE!

--- src/bubi.hpp
+++ src/bubi.hpp
@@ -1,21 +1,29 @@
 #ifndef BUBI_HPP
 #define BUBI_HPP
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+const char BUBI_NAME[] 		= "Application Bubi";  											// Application name for LCD display
+const char BUBI_VERSION[] = "Version 2.0.1";  												// Application version
+
 
 // Typdefinitionen
-typedef struct {
+typedef LiquidCrystal_I2C 													lcd_t;						// Verwendung von LiquidCrystal_I2C als lcd_t für die LCD-Instanz
+typedef enum { NOCLICK = 0, SHORTCLICK, LONGCLICK } clickType_t;			// Klick-Status als Enumeration
+typedef enum { STAY = 0, FORWARD, BACKWARD, PING } 	modeType_t;				// Programm-Modi als Enumeration
+
+
+typedef struct {																											// Globale Konstantenstruktur
 	const uint8_t		leds[8];																						// LED-Pins
 	const uint8_t		btn;																								// Button-Pin
+	const uint8_t		lcdData[3];																					// LCD I2C Adresse
 	const uint16_t	longPress;																					// Zeit in ms, die für einen Longclick benötigt wird
 	const uint8_t 	prMax;																							// Anzahl der Pattern
 	const uint16_t 	hold[4];																						// Pausen zwischen den Mustern in ms
 	const char 			*msg[4];																						// Ausgabe der Modus-Namen im Serial-Monitor
 } rock_t;
 
-typedef enum { NOCLICK = 0, SHORTCLICK, LONGCLICK } clickType_t;			// Klick-Status als Enumeration
-typedef enum { STAY = 0, FORWARD, BACKWARD, PING } modeType_t;				// Programm-Modi als Enumeration
-
-typedef struct {
+typedef struct {																											// Globale Variable für die Konfiguration
 	modeType_t 			mode;																								// aktueller Modus
 	uint8_t 				ltClick; 																						// Anzahl der Longclicks	
 	uint8_t 				output;																							// aktuelles Ausgangsmuster
@@ -51,8 +59,10 @@ class Show {																													// Input: Ausgangsmuster, Output: LED-Z
 	private:
 		config_t &rg;
 		uint8_t dat;
+		lcd_t lcd;																												// LCD instance for displaying values
 	public:
-		Show(config_t &rg) : rg(rg), dat(0x0F) {};
+		Show(config_t &rg) : rg(rg), dat(0x0F), lcd(rg.rPtr->lcdData[0], rg.rPtr->lcdData[1], rg.rPtr->lcdData[2])  {}
+		void init();
 		void update();
 };
 
@@ -86,8 +96,8 @@ void Handler::shortClick() {
 	rg.click = NOCLICK;		
 };
 
-void Handler::shortLoop() {
-	uint32_t now = millis();
+void Handler::shortLoop() {																						// Handler für die Muster, der in jedem Loop-Durchlauf aufgerufen wird.
+		uint32_t now = millis();
 	if(now < nextTime) return;
 	nextTime = now + rg.rPtr->hold[rg.mode];
 	switch (rg.mode) {
@@ -131,20 +141,45 @@ void Handler::ping() {																								// Schiebt die 1 von links nach re
 	}
 }
 
+void Show::init() {
+	lcd.init();  																												// LCD initialisieren
+	lcd.backlight();  																									// Hintergrundbeleuchtung einschalten
+	lcd.clear();  																											// LCD löschen
+	lcd.setCursor(0, 0);
+	lcd.print(BUBI_NAME);																								// Print application name on the first row
+	lcd.setCursor(0, 1);
+	lcd.print(BUBI_VERSION);																						// Print application version on the second row
+	delay(3000);  																											// Warte 3 Sekunden, damit die Informationen gelesen werden können
+	lcd.clear();  																											// LCD löschen für die Anzeige der Modus-Information
+	lcd.setCursor(0, 0);
+	lcd.print("Mode: ");  																							// Print current mode on the first row
+	lcd.setCursor(6, 0);
+	lcd.print(rg.rPtr->msg[rg.mode]);
+	lcd.setCursor(0, 1);
+	lcd.print("Output -");  																						// Print initial output pattern in binary on the second row
+}
+
 void Show::update() {
 	if(dat != rg.output) {	
 		dat = rg.output;
+	lcd.setCursor(6, 0);
+	lcd.print(rg.rPtr->msg[rg.mode]);
+	lcd.setCursor(7, 1);
 		for(uint8_t i = 0; i < 8; i++) {
 			digitalWrite(rg.rPtr->leds[i], (dat >> i) & 0x01);
 		}
-		if(dat==0) Serial.println("Output: -"); 	
+		if(dat == 0) {
+			Serial.println("Output: -"); 
+			lcd.print("-       ");  																				// Print the current output
+		}	
 		else {
-			char buffer[9];  																							// Puffer für die Binärdarstellung des Musters
+			char buffer[9];  																								// Puffer für die Binärdarstellung des Musters
 			for(uint8_t i = 0; i < 8; i++) {
-				buffer[7 - i] = (dat & (0x01 << i)) ? '\x2A' : '\x20';  		// Fülle den Puffer mit '*' oder ' ' entsprechend den Bits des Musters
+				buffer[7 - i] = (dat & (0x01 << i)) ? '\x2A' : '\x20';  			// Fülle den Puffer mit '*' oder ' ' entsprechend den Bits des Musters
 			}
-			buffer[8] = '\0';  																						// Nullterminator für die String-Ausgabe
-			Serial.println(buffer);																	
+			buffer[8] = '\0';  																							// Nullterminator für die String-Ausgabe
+			Serial.println(buffer);	
+			lcd.print(buffer);  																						// Print the current output																
 		}											
 	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include "bubi.hpp"
+//  I2C + LCD configuration
+const uint8_t I2C_Addr 			= 0x27;             											// I2C Adresse
+const uint8_t LCD_cols 			= 16;               											// LCD Spalten
+const uint8_t LCD_rows 			= 2;                											// LCD Zeilen
 
-const rock_t rock = {																							// globale Konfigurationsstruktur mit allen relevanten Parametern
+
+const rock_t rock = {																									// globale Konstanten
 	.leds 			= { GPIO_NUM_18, GPIO_NUM_3, GPIO_NUM_4, GPIO_NUM_5, GPIO_NUM_13, GPIO_NUM_14, GPIO_NUM_16, GPIO_NUM_17 },
 	.btn 				= GPIO_NUM_19,
+	.lcdData		= { I2C_Addr, LCD_cols, LCD_rows },
 	.longPress 	=	400,
 	.prMax 			= 4,
-	.hold 			= { 20, 1000, 300, 200 },
-	.msg 				= { "WAIT", "FORWARD", "BACKWARD", "PINGPONG" }
+	.hold 			= { 20, 800, 500, 300 },
+	.msg 				= { "WAIT     ", "FORWARD ", "BACKWARD", "PINGPONG" }
 };
 
-config_t co = {																										// globale Konfigurationsstruktur mit allen relevanten Parametern
+config_t co = {																												// globale Variable - wird in den Klassen als Referenz übergeben
 	.mode 			= STAY,
 	.ltClick 		= 0,
 	.output 		= 0,
@@ -17,11 +23,11 @@ config_t co = {																										// globale Konfigurationsstruktur mit a
 	.rPtr 			= &rock
 };
 
-Button button(co);  																							// Button-Objekt mit Referenz auf Konfigurationsstruktur
-Handler handler(co);  																						// Handler-Objekt mit Referenz auf Konfigurationsstruktur
-Show show(co);                                          					// Show-Objekt mit Referenz auf Konfigurationsstruktur
+Button button(co);  																									// Button-Objekt mit Referenz auf Konfigurationsstruktur
+Handler handler(co);  																								// Handler-Objekt mit Referenz auf Konfigurationsstruktur
+Show show(co);                                          							// Show-Objekt mit Referenz auf Konfigurationsstruktur
 
-void init(const rock_t *rPtr) {																		// Alle Pins initialisieren, wird in setup() aufgerufen
+void init(const rock_t *rPtr) {																				// Alle Pins initialisieren, wird in setup() aufgerufen
 	pinMode(rPtr->btn, INPUT_PULLUP);
 	for(uint8_t i = 0; i < 8; i++) {
 		pinMode(rPtr->leds[i], OUTPUT);
@@ -31,16 +37,17 @@ void init(const rock_t *rPtr) {																		// Alle Pins initialisieren, wi
 
 void setup() {
 	Serial.begin(115200);
-	delay(1000);  																									// Warte auf Serial-Monitor
-	Serial.println("\nBubi gestartet.\nAktueller Modus: " + String(rock.msg[co.mode]));  		// Ausgabe des Startmodus im Serial-Monitor
-	init(&rock);  																									// Initialisierung der Pins
+	delay(1000);  																											// Warte auf Serial-Monitor
+	Serial.println("\nBubi gestartet.\nAktueller Modus: " + String(rock.msg[co.mode]));  		
+	init(&rock); 
+	show.init(); 																												// Initialisierung LCD und Anzeige der Startinformationen
 }
 
 void loop() {
 	button.update();
 	handler.update();
 	show.update();
-	delay(10);  																										// Kurze Pause, um CPU-Last zu reduzieren
+	delay(10);  																												// Kurze Pause, um CPU-Last zu reduzieren
 }
 
 
